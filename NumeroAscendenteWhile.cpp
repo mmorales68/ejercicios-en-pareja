ea7206f4e7be3cc39daf178609c13fd8b4852768
mmorales68 <miguelm4032@gmail.com>
2024-02-24 09:54:19 -0600

    Ejercicio #1 Buques While Miguel Morales 0909-23-14463/Jefferson Soto 0909-23-11789

---
+++ NumeroAscendenteWhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    cout << "Bienvenido al programa que calcula los numeros del 1 al 20, a continuacion se le mostrara dicha indicacion" << endl;
+    int numero = 1;
+
+    while (numero <= 20) {
+        cout << numero << endl;
+        numero++;
+    }
+    cout << "Pase feliz dia" << endl;
+    return 0;
+}
+
+
